27307dde6e513612664b89f1b5a2eb2929489f24
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2016-09-14 19:57:30 +0200

    Tablename

--- filedb/lib/file_db/database.rb
+++ filedb/lib/file_db/database.rb
@@ -1,6 +1,17 @@
+require 'json'
+
 module FileDb
   class Database
+
     def initialize(data_file)
+      file = File.read(data_file)
+      @data = JSON.parse(file)
     end
+
+
+    def table_names
+      @data.keys.sort
+    end
+
   end
 end
